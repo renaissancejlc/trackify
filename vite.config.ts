27a800517a4fdb9d04be8fa4985cd5be0a46b952
renaissancejlc <91338859+renaissancejlc@users.noreply.github.com>
2025-05-27 13:11:57 -0700

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import mkcert from 'vite-plugin-mkcert'
 import path from 'path'
 
 export default defineConfig({
-  plugins: [react(), mkcert()],
-  server: {
-    https: true,
-  },
+  plugins: [react()],
+  server: {},
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
